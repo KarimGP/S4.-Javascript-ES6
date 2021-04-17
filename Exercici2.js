a353a585628ab6794731ce492e3ae7c45e8a2076
unknown <karimkgp@gmail.com>
2021-04-17 18:58:54 +0200

    exercici 2

---
+++ Exercici2.js
@@ -0,0 +1,25 @@
+//ARREGLAR ERROR
+
+/*users.map(function (user) {
+    user.firstName;
+ });
+ 
+ var users = 
+ [{ firstName: 'Homer', lastName: 'Simpson' },
+ { firstName: 'Marge', lastName: 'Simpson' },
+ { firstName: 'Bart', lastName: 'Simpson' },
+ { firstName: 'Lisa', lastName: 'Simpson' },
+ { firstName: 'Maggie', lastName: 'Simpson' }];*/
+
+ var users = 
+ [{ firstName: 'Homer', lastName: 'Simpson' },
+ { firstName: 'Marge', lastName: 'Simpson' },
+ { firstName: 'Bart', lastName: 'Simpson' },
+ { firstName: 'Lisa', lastName: 'Simpson' },
+ { firstName: 'Maggie', lastName: 'Simpson' }];
+
+ 
+ let usersFirstName = users.map((user) => {
+    return user.firstName;
+ });
+console.log(usersFirstName);
